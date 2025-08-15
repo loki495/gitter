7082814ce406d3ec62f19c85aa1642a1363843a5
Andres Crucitti <dasc495@gmail.com>
2025-08-15 13:24:51 -0700

    basic misc

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-use App\Models\PracticeSet;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
 Route::middleware(['auth', 'verified'])->group(function () {
-    Volt::route('dashboard', 'admin.dashboard')->name('dashboard');
+    Volt::route('/', 'admin.dashboard')->name('dashboard');
     Volt::route('sites', 'admin.sites.index')->name('sites.index');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
