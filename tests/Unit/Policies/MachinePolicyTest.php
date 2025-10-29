fc15b96764cf881b46a33ffbcbd7bcc6d1482466
Andres Crucitti <dasc495@gmail.com>
2025-10-28 19:04:51 -0700

    rearrange tests

---
+++ tests/Unit/Policies/MachinePolicyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Machine;
+use App\Models\User;
+use App\Policies\MachinePolicy;
+
+beforeEach(function () {
+    $this->only();
+});
+
+it('allows a user to view, update, delete their own machine', function () {
+    $user = User::factory()->create();
+    $machine = Machine::factory()->create(['created_by' => $user->id]);
+
+    $policy = new MachinePolicy();
+
+    expect($policy->view($user, $machine))->toBeTrue()
+        ->and($policy->update($user, $machine))->toBeTrue()
+        ->and($policy->delete($user, $machine))->toBeTrue();
+});
+
+it('denies a user from viewing, updating, deleting machines they do not own', function () {
+    $user = User::factory()->create();
+    $otherUser = User::factory()->create();
+    $machine = Machine::factory()->create(['created_by' => $otherUser->id]);
+
+    $policy = new MachinePolicy();
+
+    expect($policy->view($user, $machine))->toBeFalse()
+        ->and($policy->update($user, $machine))->toBeFalse()
+        ->and($policy->delete($user, $machine))->toBeFalse();
+});
+
+it('allows any user to create a machine', function () {
+    $user = User::factory()->create();
+    $policy = new MachinePolicy();
+
+    expect($policy->create($user))->toBeTrue();
+});
+
