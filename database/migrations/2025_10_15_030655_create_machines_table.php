53b92e5fe7ef84eeb3699405e59a0c0aed7cb81e
Andres Crucitti <dasc495@gmail.com>
2025-11-01 15:55:57 -0700

    machines: remove password, make ip, ssh key, and user required

--- database/migrations/2025_10_15_030655_create_machines_table.php
+++ database/migrations/2025_10_15_030655_create_machines_table.php
@@ -12,15 +12,14 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('type'); // work/staging/production
-            $table->string('ip')->nullable();
+            $table->string('ip');
             $table->string('ssh_user');
             $table->integer('ssh_port')->default(22);
-            $table->string('ssh_key_path')->nullable();
-            $table->text('ssh_password_encrypted')->nullable();
             $table->text('notes')->nullable();
             $table->timestamps();
 
-            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->constrained('users')->nullOnDelete();
+            $table->foreignId('ssh_key_id')->constrained('ssh_keys')->nullOnDelete();
 
             $table->index('ip'); // optional
         });
