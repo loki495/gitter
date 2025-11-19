2ad92ee5843e31035ac27610accd340d02fbf7f3
Andres Crucitti <dasc495@gmail.com>
2025-11-18 17:45:03 -0800

    remove unnecessary failing test

--- tests/Unit/Actions/Git/BaseActionTest.php
+++ tests/Unit/Actions/Git/BaseActionTest.php
@@ -5,39 +5,7 @@ declare(strict_types=1);
 namespace Tests\Unit\Actions\Git;
 
 use App\Actions\Git\Status;
-use App\Models\Deployment;
-use App\Models\Machine;
-use App\Models\User;
 use App\Services\GitService;
-
-beforeEach(function (): void {
-    $this->user = User::factory()->create();
-    $this->actingAs($this->user);
-});
-
-it('gets status successfully', function (): void {
-    $machine = Machine::factory()->create([
-        'user_id' => $this->user->id,
-        'name' => 'localhost',
-        'ip' => '',
-        'ssh_key_id' => null,
-        'ssh_user' => null,
-    ]);
-
-    $deployment = Deployment::factory()->create([
-        'user_id' => $this->user->id,
-        'machine_id' => $machine->id,
-        'path' => '/home/andres/www/git',
-    ]);
-
-    $git = app(GitService::class);
-    $result = $git->status
-        ->addArgument('--short')
-        ->execute($deployment);
-
-    expect($result->success())->toBeTrue();
-    expect($result->result())->toBeString();
-});
 
 it('throws_runtime_exception_if_deployment_is_not_found', function (): void {
     $this->expectException(\RuntimeException::class);
